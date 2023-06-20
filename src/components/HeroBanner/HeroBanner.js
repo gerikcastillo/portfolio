334264b87f08751ce0f7a2abce30b428d0cac471
Gerik Castillo <gcast359@Geriks-MBP.lan>
2023-06-20 12:19:36 -0400

    added new images and logos and styled hero banner and nav bar

--- src/components/HeroBanner/HeroBanner.js
+++ src/components/HeroBanner/HeroBanner.js
@@ -1,20 +1,42 @@
 import "./HeroBanner.scss";
+import GithubLogo from "../../assets/images/githublogo.png";
+import LinkedInLogo from "../../assets/images/linkedinlogo.png";
+import GerikImage from "../../assets/images/gerikimage.png";
 
 function HeroBanner() {
   return (
     <>
       <section className="hero">
         <div className="hero__container">
+        <div className="hero__text-container">
           <div className="hero__title-wrapper">
-            <p className="hero__title">Hello!</p>
-            <p className="hero__title">I'm Gerik,</p>
-            <p className="hero__title">a full-stack web developer</p>
+            <p className="hero__title-1">HI THERE 👋</p>
+            <p className="hero__title-2">I'M GERIK</p>
+            <p className="hero__title-3">DEVELOPER + DESIGNER</p>
+            <p className="hero__subtitle">
+              with a keen eye for design and bringing products to life.
+            </p>
           </div>
-          <div className="hero__subtitle-wrapper">
-            <p className="hero__subtitle">with a keen eye for design and bringing products to life.</p>
+          <div className="hero__links-wrapper">
+            <a href="#" target="_blank">
+              <img
+                className="hero__logo"
+                src={LinkedInLogo}
+                alt="linkedin"
+              ></img>
+            </a>
+            <a href="#" target="_blank">
+              <img
+                className="hero__logo"
+                src={GithubLogo}
+                alt="github"
+              ></img>
+            </a>
           </div>
-          <a href="#" target="_blank"><img src="" alt="linkedin"></img></a>
-          <a href="#" target="_blank"><img src="" alt="github"></img></a>
+        </div>
+        <div className="hero__image-container">
+            <img className="hero__image" src={GerikImage}alt="gerik"></img>
+        </div>
         </div>
       </section>
     </>
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -5,10 +5,10 @@ function NavBar() {
     <>
       <nav className="navbar">
         <div className="navbar__container">
-          <button className="navbar__button">About</button>
-          <button className="navbar__button">Experience</button>
-          <button className="navbar__button">Projects</button>
-          <button className="navbar__button">Contact</button>
+          <button className="navbar__button">ABOUT</button>
+          <button className="navbar__button">EXPERIENCE</button>
+          <button className="navbar__button">PROJECTS</button>
+          <button className="navbar__button">CONTACT</button>
           <button className="navbar__button navbar__button--border">Resume</button>
         </div>
       </nav>
